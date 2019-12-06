d196b758e29abfe709fcfd43abd982d5c4ca8b57
David Fares <david@dsfares.com>
2019-12-06 17:18:38 -0600

    More Portables Fonts

--- template-parts/menu-boards/menu-layout/menu-parts/menu-list/section-title.php
+++ template-parts/menu-boards/menu-layout/menu-parts/menu-list/section-title.php
@@ -1,5 +1,5 @@
 <h1 class="section-title">
-  <div class="title"><div class="title-text"><?php echo get_sub_field('section_title'); ?></div><div class="title-line"></div></div>
+  <div class="title"><div class="title-text"><?php echo get_sub_field('section_title'); ?></div></div>
   <?php $section_subtitle = get_sub_field('section_subtitle');
     if ($section_subtitle) { ?>
       <div class="subtitle"><?php echo get_sub_field('section_subtitle'); ?></div>
