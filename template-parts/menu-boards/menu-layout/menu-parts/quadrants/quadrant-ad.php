e0a268d2bd86c04a9f60f8655c0603ebf654a01f
David Fares <david@dsfares.com>
2019-12-19 14:17:16 -0600

    Ad Slider Code (Testing)

--- template-parts/menu-boards/menu-layout/menu-parts/quadrants/quadrant-ad.php
+++ template-parts/menu-boards/menu-layout/menu-parts/quadrants/quadrant-ad.php
@@ -1,9 +1,13 @@
 <div class="quadrant quadrant-ad">
   <div class="quadrant-wrap">
     <div class="featured-ad-container">
-      <?php
+      <?php if get_field('force_ad_slider', 'options') {
+        $ad_list = get_field('ad_list', 'options');
+    		$count = 0;
+        include( locate_template( 'template-parts/menu-boards/menu-layout/menu-parts/advertisements/ad-slider.php', false, false ) );
+      } else {
         include( locate_template( 'template-parts/menu-boards/menu-layout/menu-parts/advertisements/featured-ad.php', false, false ) );
-      ?>
+      } ?>
     </div>
   </div>
 </div>
